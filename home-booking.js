679975066ea1445821eb8d1a99506da7c1d7d5f0
william-jan <janwilliam3770@gmail.com>
2025-12-18 18:44:35 +0700

    pengerjaan modal dan modal konfirmasi

--- home-booking.js
+++ home-booking.js
@@ -1,41 +1,24 @@
-// Ambil Data
-const overlay = document.querySelector(".modal-overlay");
-const bookingModal = document.getElementById("bookingModal");
-
-// Untuk Buka Modal
 const bookingButtons = document.querySelectorAll(".booking-btn");
+const bookingModal = document.getElementById("bookingModal");
 const doctorInput = document.getElementById("doctorName");
 const closeBooking = document.getElementById("closeBooking");
 
+// untuk tampilin modal dan auto ketik nama dokter di form modal
 bookingButtons.forEach(function (btn) {
     btn.addEventListener("click", function () {
         doctorInput.value = btn.dataset.doctor;
 
-        // tampilkan overlay
-        overlay.style.display = "block";
-        overlay.style.opacity = "1";
-
-        // tampilkan modal booking
         bookingModal.style.opacity = "1";
         bookingModal.style.pointerEvents = "auto";
         bookingModal.style.transform = "translate(-50%, -50%) scale(1)";
     });
 });
 
-// Tutup Modal Booking
-closeBooking.addEventListener("click", closeBookingModal);
-overlay.addEventListener("click", closeBookingModal);
-
-function closeBookingModal() {
+// untuk tutup modal
+closeBooking.addEventListener("click", function () {
     bookingModal.style.opacity = "0";
     bookingModal.style.pointerEvents = "none";
     bookingModal.style.transform = "translate(-50%, -40%) scale(0.98)";
-
-    overlay.style.opacity = "0";
-
-    setTimeout(function () {
-        overlay.style.display = "none";
-    }, 200);
-}
+});
 
 
